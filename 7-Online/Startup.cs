d85bc18b0092d278935e313f72658f7b4b062330
Misael.Vega <Misael.Vega@SISTEMAS3>
2018-11-16 15:30:35 -0700

    Add project files.

---
+++ 7-Online/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_7_Online.Startup))]
+namespace _7_Online
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
